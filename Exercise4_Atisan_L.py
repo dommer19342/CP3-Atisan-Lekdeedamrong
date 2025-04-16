ccf9f0e0a27fdb75c08b0df387d19b30e1cb32b9
dommer19342 <athisan1999@gmail.com>
2025-04-16 13:17:35 +0700

    Exercise4_Atisan_L
    Exercise4_Atisan_L

---
+++ Exercise4_Atisan_L.py
@@ -0,0 +1,13 @@
+#declare variables
+FE = 80.5
+GB = 80.7
+ICS = 80.8
+CP = 80.9
+
+#print score
+
+print("--- Your score ---")
+print("Foundation English               = ",FE)
+print("General Business                 = ",GB)
+print("Introduction to Computer Systems = ",ICS)
+print("Computer Programming             = ",CP)
